a5f72a1755f3e974333c9a872fb90486afa36583
olegsuv <olegsuv@gmail.com>
2022-11-29 02:08:24 +0100

    Small adjustments

--- src/Autocomplete/Autocomplete.tsx
+++ src/Autocomplete/Autocomplete.tsx
@@ -70,7 +70,7 @@ const Autocomplete: FunctionComponent<AutocompleteProps> = ({
         setActiveSuggestion(activeSuggestion + 1);
       }
     },
-    [userInput]
+    [userInput, activeSuggestion]
   );
 
   const highlightedText = (text, subString) =>
